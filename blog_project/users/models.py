c26816027b39825a88723c6c65ecbef95be50e09
Bibolat2005 <nur650biko@gmail.com>
2024-10-02 11:03:20 +0500

    basic concepts are created

---
+++ blog_project/users/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pics', blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Follow(models.Model):
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following')
+    following = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followers')
+
+    def __str__(self):
+        return f'{self.follower} follows {self.following}'
